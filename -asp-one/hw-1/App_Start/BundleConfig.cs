88a7c31182587c507301713d995f6564d7eda0a5
Artem Gabatawr <gabatawr@gmail.com>
2021-06-29 16:22:11 +0300

    save

---
+++ -asp-one/hw-1/App_Start/BundleConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace hw_1
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/pe-icon.css",
+                      "~/Content/grid.css",
+                      "~/Content/magnific-popup.min.css",
+                      "~/Content/swiper.css",
+                      "~/Content/main.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/vendors").Include(
+                        "~/Scripts/jquery.min.js",
+                        "~/Scripts/imagesloaded.pkgd.js",
+                        "~/Scripts/isotope.pkgd.js",
+                        "~/Scripts/jquery.nav.min.js",
+                        "~/Scripts/jquery.easing.min.js",
+                        "~/Scripts/jquery.matchHeight.min.js",
+                        "~/Scripts/jquery.magnific-popup.min.js",
+                        "~/Scripts/masonry.pkgd.js",
+                        "~/Scripts/jquery.waypoints.min.js",
+                        "~/Scripts/swiper.jquery.js",
+                        "~/Scripts/menu.js",
+                        "~/Scripts/typed.min.js",
+                        "~/Scripts/main.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                        "~/Scripts/main.js"));
+        }
+    }
+}
---
+++ -asp-one/hw-1/Controllers/MainController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hw_1.Controllers
+{
+    public class MainController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Blog_detail()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Work()
+        {
+            return View();
+        }
+        public ActionResult Work_detail()
+        {
+            return View();
+        }
+    }
+}
