bcc0d6e59f3ed652d7658df79a20efbcd5b29576
Gabatawr <gabatawr@gmail.com>
2021-07-07 21:00:42 +0300

    save

--- -asp-one/mvc/Data/ApplicationDbContext.cs
+++ -asp-one/mvc/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 namespace mvc.Data
 {
@@ -11,6 +12,23 @@ namespace mvc.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
+        }
+
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Book>(b =>
+            {
+                b.Property(e => e.Codes).HasConversion(
+                    array => JsonSerializer.Serialize(array, null),
+                    str => JsonSerializer.Deserialize<string[]>(str, null)
+                );
+            });
         }
     }
 }
---
+++ -asp-one/mvc/Data/Models.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc.Data
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public String Title { get; set; }
+        public String Decription { get; set; }
+        public String[] Codes { get; set; }
+        public HashSet<Author> Authors { get; set; }
+    }
+
+    public class Author
+    {
+        public Guid Id { get; set; }
+        public String FirstName { get; set; }
+        public DateTime Birthday { get; set; }
+        public HashSet<Book> Books { get; set; }
+    }
+}
