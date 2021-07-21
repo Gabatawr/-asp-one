4f94f12b264edfe72e91a94048bfa08c25b12d73
Artem Gabatawr <gabatawr@gmail.com>
2021-07-21 14:28:10 +0300

    add files

---
+++ -asp-one/mvc/Data/Migrations/20210721075306_valid.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mvc.Data.Migrations
+{
+    public partial class valid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Country",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Country",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64,
+                oldNullable: true);
+        }
+    }
+}
---
+++ -asp-one/mvc/ViewComponents/LanguageSwitcherViewComponent .cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using mvc.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc.ViewComponents
+{
+    public class LanguageSwitcherViewComponent : ViewComponent
+    {
+        private readonly IOptions<RequestLocalizationOptions> _localizationOptions;
+
+        public LanguageSwitcherViewComponent(IOptions<RequestLocalizationOptions> localizationOptions)
+        {
+            _localizationOptions = localizationOptions;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var cultureFeature = HttpContext.Features.Get<IRequestCultureFeature>();
+            var languageSwitcherModel = new LanguageSwitcherModel
+            {
+                SupportedCultures = _localizationOptions.Value.SupportedCultures.ToList(),
+                CurrentUICulture = cultureFeature.RequestCulture.Culture,
+                CurrentSitecoreRoute = this.HttpContext.Request.RouteValues["sitecoreRoute"]?.ToString() ?? ""
+            };
+            return View(languageSwitcherModel);
+        }
+    }
+}
